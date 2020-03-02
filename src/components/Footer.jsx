34943c9e501d79037798dc1e38cb902b8a78128d
Imo-Hetherington <hetherington.ir@gmail.com>
2020-03-02 17:07:09 +0000

    Updated footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className="Footer">Footer info here</footer>;
+  return (
+    <footer className="Footer">
+      <p>Made by Imogen Hetherington</p>
+      <p>
+        <a href="https://github.com/Imo-Hetherington/nc-news">
+          View project on GitHub
+        </a>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
